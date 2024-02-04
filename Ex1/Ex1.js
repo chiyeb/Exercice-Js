40e5a5bc156fcfcbeff0732a7bbf4fc1bfd3e53f
chiheb bradai <119317441+chiyeb@users.noreply.github.com>
2024-02-04 15:27:55 +0100

    Add files via upload

--- Ex1/Ex1.js
+++ Ex1/Ex1.js
@@ -2,25 +2,27 @@ function calcul(nb1, nb2, op){
     switch (op){
         case "+":
             return nb1 + nb2;
-            break;
         case "-":
             return nb1 - nb2;
-            break;
         case "*":
             return nb1 * nb2;
-            break;
         case "/":
-            if (nb2 == 0){
+            if (nb2 === 0){
                 return "erreur";
             }
             return nb1 / nb2;
-            break;
     }
 }
+
 function recupEntry(){
-    var nb1 = document.getElementsByName("number1")[0].value;
-    var nb2 = document.getElementsByName("number2")[0].value;
-    var operator = document.getElementsByName("operator")[0].value;
-    var result = calcul(parseFloat(nb1), parseFloat(nb2), operator)
-    document.getElementsByName("result")[0].textContent = result;
+    var nb1 = parseFloat(document.getElementById("number1").value);
+    var nb2 = parseFloat(document.getElementById("number2").value);
+    var operator = document.getElementById("operator").value;
+
+    if (!isNaN(nb1) && !isNaN(nb2)) {
+        var result = calcul(nb1, nb2, operator);
+        document.getElementById("result").innerHTML = result;
+    } else {
+        document.getElementById("result").textContent = "Entrées non valides";
+    }
 }
